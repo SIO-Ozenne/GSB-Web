f944ac12305de0b88184e41e202e0899407ce460
William Haskell <will09600@gmail.com>
2017-12-04 13:26:13 +0100

    Sidebar interactive + fond d'écran

--- site statique/scripts.js
+++ site statique/scripts.js
@@ -1,11 +1,15 @@
-function w3_open() {
-    document.getElementById("main").style.marginLeft = "10%";
-    document.getElementById("sidebarDesFamilles").style.width = "10%";
-    document.getElementById("sidebarDesFamilles").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
-}
-function w3_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("sidebarDesFamilles").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
+function w3_toggle() {
+    if(document.getElementById("sidebarDesFamilles").style.display == "none")
+    {
+        document.getElementById("main").style.marginLeft = "11%";
+        document.getElementById("sidebarDesFamilles").style.width = "11%";
+        document.getElementById("sidebarDesFamilles").style.display = "block";
+        document.getElementById("toggleBtn").innerHTML = "&times;";
+    }
+    else
+    {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("sidebarDesFamilles").style.display = "none";
+        document.getElementById("toggleBtn").innerHTML = "&#9776;";
+    }
 }
