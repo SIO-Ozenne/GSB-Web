110e431d9d2bc2e25c7fc75f6e837121fd3fd454
William Haskell <will09600@gmail.com>
2017-12-14 11:18:15 +0100

    Mise en forme CSS / alignement d'images ghetto

--- site statique/scripts.js
+++ site statique/scripts.js
@@ -1,15 +1,15 @@
 function w3_toggle() {
-    if(document.getElementById("sidebarDesFamilles").style.display == "none")
+    if(document.getElementById("sidebar").style.display == "none")
     {
         document.getElementById("main").style.marginLeft = "11%";
-        document.getElementById("sidebarDesFamilles").style.width = "11%";
-        document.getElementById("sidebarDesFamilles").style.display = "block";
+        document.getElementById("sidebar").style.width = "11%";
+        document.getElementById("sidebar").style.display = "block";
         document.getElementById("toggleBtn").innerHTML = "&times;";
     }
     else
     {
         document.getElementById("main").style.marginLeft = "0%";
-        document.getElementById("sidebarDesFamilles").style.display = "none";
+        document.getElementById("sidebar").style.display = "none";
         document.getElementById("toggleBtn").innerHTML = "&#9776;";
     }
 }
